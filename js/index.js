dd9bd0eea0f42d83b02ac633e3e2d15a255c99a3
Ezequiel Vulcano <110461102+vulky777@users.noreply.github.com>
2024-04-12 14:24:48 -0300

    cargo repo
    Antes de agregarle PHP

--- js/index.js
+++ js/index.js
@@ -1,81 +1,5 @@
-//! CARRUSEL de Index
-
-let conocenosCarrusel = document.querySelector(".carrusel-conocenos")
-let tituloCarrusel = document.querySelector(".carrusel-titulo")
-let ofertaCarrusel = document.querySelector(".carrusel-oferta")
-let carruselFondo = document.querySelector(".carrusel")
-let carruselFondo2 = document.querySelector(".carrusel2")
-let redondo = document.querySelectorAll(".btn > div")
-let contador = 1 //? Contador para las repeticiones del carrusel
-
-let carruselIndex = [
-    {
-        oferta: "*Solo hoy ofrecemos envío gratis",
-        titulo: "Tendencias de la moda",
-        conocenos: "Conoce la coleccion",
-        fondo: 'url("../imagenes/seccion2/cart-1.jpg")',
-        id: "a"
-    },
-    {
-        oferta: "*Solo hoy ofrecemos envío gratis",
-        titulo: "Moda de mujer",
-        conocenos: "Conoce la coleccion",
-        fondo: 'url("../imagenes/seccion2/cart-2.jpg")',
-        id: "b"
-    },
-    {
-        oferta: "*Solo hoy ofrecemos envío gratis",
-        titulo: "Lo ultimo en moda",
-        conocenos: "Conoce la coleccion",
-        fondo: 'url("../imagenes/seccion2/cart-3.png")',
-        id: "b"
-    }
-]
-
-// Genero la animacion del carrusel para cambiar el contenido cada 3 segundos
-
-function carrusel1(){
-
-    if(contador < 3){
-        
-        carruselFondo.style.background = carruselIndex[contador].fondo
-        carruselFondo.style.left = contador * 100;
-        ofertaCarrusel.innerText = carruselIndex[contador].oferta
-
-        tituloCarrusel.innerText = carruselIndex[contador].titulo
-
-        conocenosCarrusel.innerText = carruselIndex[contador].conocenos
-        contador++
-        mostrarRedondo(contador, redondo)
-    } else {
-        contador = 0
-    }
-}
-
-function mostrarRedondo(x, objeto){
-    objeto.forEach((el) => {
-        el.style.opacity = 0
-    })
-
-    redondo[x - 1].style.opacity = 1
-}
-
-function carruselDefault(){
-    carruselFondo.style.background = carruselIndex[0].fondo
-    ofertaCarrusel.innerText = carruselIndex[0].oferta
-    tituloCarrusel.innerText = carruselIndex[0].titulo
-    conocenosCarrusel.innerText = carruselIndex[0].conocenos
-    mostrarRedondo(contador, redondo)
-    carruselFondo.style.left = 0
-}
-
-carruselDefault()
-setInterval(carrusel1,3000)
-
-// Evento que se encarga de cambiar el orden del carrusel por los botones
-
-//! PRODUCTOS NUEVOS
-
+// ! ---------------- OBJETOS ---------------- ! //
+// Productos de muestra del index  
 let productosNuevos = [
     {
         nombre: "Saco de vestir blanco",
@@ -94,7 +18,7 @@ let productosNuevos = [
     {
         nombre: "Conjunto Homegir",
         precio: "$ 43.000",
-        catalogo: "mujeres",
+        catalogo: "mujer",
         foto: "./imagenes/seccion3/producto-3.jpg",
         id: "c"
     },
@@ -121,10 +45,25 @@ let productosNuevos = [
     }
 ]
 
+
+
+// ! ---------------- VARIABLES ---------------- ! //
+// Genero los filtros del index     
+let mujer = document.querySelector("#mujer");
+let hombre = document.querySelector("#hombre");
+let categorias = document.querySelectorAll(".categorias");
+let productosAgregados = document.querySelectorAll(".productos-agregados");
+
+// CARRUSEL DE TESTIMONIOS
+let contenedorPadre = document.querySelector(".contenedor-padre")
+let parrafosHijos = document.querySelectorAll(".hijo")
+let contadorParrafos = 1
+
+
+
+// ! ---------------- FUNCIONES ---------------- ! //
 // Genero el catalogo de productos del index
-
 productosNuevos.forEach((el) => {
-
 
     //? Contenedor Final
     let contenedorProductosNuevos = document.querySelector(".contenedor-pruductos-nuevos")
@@ -152,7 +91,7 @@ productosNuevos.forEach((el) => {
 
     let div2 = document.createElement("div")
     div2.setAttribute("data-categoria", el.catalogo)
-    div2.setAttribute("class", "productos-agregados")
+    div2.setAttribute("class", "productos-agregados col-12 col-sm-6 col-md-4 p-4")
 
     
     // Organizo los elementos
@@ -167,24 +106,13 @@ productosNuevos.forEach((el) => {
     contenedorProductosNuevos.appendChild(div2)
 })
 
-
-// Genero los filtros del index     
-
-let mujer = document.querySelector("#mujer");
-let hombre = document.querySelector("#hombre");
-let categorias = document.querySelectorAll(".categorias");
-let productosAgregados = document.querySelectorAll(".productos-agregados");
-
 //Funcion que se encarga de filtar los elementos por categorias
-
 function filtrarCatalagosIndex(x) {
     // x = categoria a filtrar
 
     x.addEventListener("click", function() {
         let valorActual = x.dataset.categoria
-        // valorActual = nombre de la categoria
 
-        // productosAgregados = todas las tarjetas de productos
         productosAgregados.forEach((el)=>{
             el.style.display = "none"
             if(el.dataset.categoria == valorActual){
@@ -198,12 +126,7 @@ filtrarCatalagosIndex(mujer);
 filtrarCatalagosIndex(hombre);
 
 
-//! Creo el carrusel que contiene los parrafos
-
-let contenedorPadre = document.querySelector(".contenedor-padre")
-let parrafosHijos = document.querySelectorAll(".hijo")
-let contadorParrafos = 1
-
+//Funcion que se encarga crear el carrusel de TESTIMONIOS
 function carrusel2(){
     if(contadorParrafos >= parrafosHijos.length){
         contenedorPadre.style.transition = "none"
