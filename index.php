28f7a520456d9914f8159e030fe790ef8a3ae306
Ezequiel Vulcano <110461102+vulky777@users.noreply.github.com>
2024-04-12 15:10:36 -0300

    cambio secciones php

--- index.php
+++ index.php
@@ -47,13 +47,13 @@
             <div class="col-md-9 mt-md-5 d-md-block d-none col-lg-7 d-flex">
                 <ul class="d-flex p-0 ">
                     <li>
-                        page 1
+                        Inicio
                     </li>
                     <li>
-                        page 1
+                        Productos
                     </li>
                     <li>
-                        page 1
+                        Contacto
                     </li>
                     <li>
                         page 1
@@ -73,7 +73,7 @@
     </header>
 
     <main class="container-fluid index">
-        
+        <?php  require_once("vistas/seccion1.php") ?>
     </main>
 
     <footer class="mt-5 pt-4 container-fluid">
@@ -148,10 +148,6 @@
         
     </footer>
 
-
-
-
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <!--ANIMACION AOS-->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
---
+++ vistas/seccion2.php
@@ -0,0 +1,1 @@
+<h2>Hola soy otra seccion</h2>
